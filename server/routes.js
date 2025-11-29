746061e85d0b1417adbc8f4eb588427623b2e5b7
tejaschaudhari131 <you@example.com>
2025-11-29 14:05:26 +0530

    'update'

--- server/routes.js
+++ server/routes.js
@@ -161,6 +161,7 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
+    family: 4, // Force IPv4
     logger: true,
     debug: true
 });
